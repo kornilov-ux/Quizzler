b2c0ed9494e9237102aa56d542aece0aa0fa4484
Alex Kornilov <alex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.comalex.kornilovv2001@gmail.com>
2023-08-05 16:59:18 +0300

    created the storyboard and add media files

---
+++ 9.Quizzler/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  9.Quizzler
+//
+//  Created by Alex  on 05.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+	
+	
+	
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+	
+
+
+}
+
